9623027adbe4249c59f6876057fdbf0d2785469d
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-22 22:48:51 +0200

    style config ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: "hsl(0, 0%, 20%)",
+        "dark-grey": "hsl(0, 0%, 12%)",
+        black: "hsl(0, 0%, 8%)",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        p: "14px",
+      },
+      fontWeight: {
+        normal: 400,
+        "semi-bold": 600,
+        bold: 700,
+      },
+    },
   },
   plugins: [],
 };
